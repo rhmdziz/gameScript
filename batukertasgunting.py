da192933092a29e0285f644a329660f8a0445799
Aziz Rahmad Isnanto <67959171+rhmdziz@users.noreply.github.com>
2021-03-26 13:07:05 +0700

    Update batukertasgunting.py

--- batukertasgunting.py
+++ batukertasgunting.py
@@ -1,20 +1,54 @@
 import random
 def bgk():
-    print('BATU KERTAS GUNTING')
-    bgk = ['batu','gunting','kertas']
-    bot = random.choice(bgk)
+    # intro
+    print('BATU KERTAS GUNTING MELAWAN BOT')
+    element = ['batu','gunting','kertas']
+    bot = random.choice(element)
     print('b = batu\nk = kertas\ng = gunting')
     ply = str(input('Masukan pilihanmu (b/k/g): '))
-    if ply == 'b':
+    
+    # konversi
+    if ply == 'b' or ply == 'B':
         ply = 'batu'
-    elif ply == 'k':
+    elif ply == 'k' or ply == 'K':
         ply = 'kertas'
-    elif ply == 'g':
+    elif ply == 'g' or ply == 'G':
         ply = 'gunting'
+    else:
+      	print('masukkan opsi yang benar\n')
+    	  
+    menang = '=> selamat anda menang\n'
+    kalah  = '=> maaf anda kalah\n'
+    
+    # algoritma batu gunting kertas
     if bot == ply:
-        print(ply,'\nvs\n',bot)
-        print('S e r i')
-    elif 
+        print(ply,'vs',bot)
+        print('=> s e r i\n')
+        
+    elif bot == 'batu' and ply == 'kertas':
+    	  print(ply,'vs',bot)
+    	  print(menang)
+    elif bot == 'gunting' and ply == 'batu':
+    	  print(ply,'vs',bot)
+    	  print(menang)
+    elif bot == 'kertas' and ply == 'gunting':
+    	  print(ply,'vs',bot)
+    	  print(menang)
+    
+    elif bot == 'kertas' and ply == 'batu':
+    	  print(ply,'vs',bot)
+    	  print(kalah)
+    elif bot == 'batu' and ply == 'gunting':
+    	  print(ply,'vs',bot)
+    	  print(kalah)
+    elif bot == 'gunting' and ply == 'kertas':
+    	  print(ply,'vs',bot)
+    	  print(kalah)
+    	  
+    # looping
+    loop = str(input('mulai lagi? y/n: '))
+    if loop == 'y' or loop == 'Y':
+    	  bgk()
+    else:
+    	print('terima kasih')
 bgk()
-
-# bentar gan bikin algonya dulu
